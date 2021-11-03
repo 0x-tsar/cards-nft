626a74c7ea55eacfcabe1a6100ed1b2f581d3baf
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-03 03:44:52 -0300

    solving crazy bugs

--- src/pages/searcher.jsx
+++ src/pages/searcher.jsx
@@ -1,41 +1,92 @@
-import React, { useRef, useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../providers/context";
 import styled from "styled-components";
 import Card from "../Components/Card";
-import { AuthContext } from "../providers/context";
 
 export const Container = styled.div`
+  /* background-color: #1f262f; */
+  color: white;
+  /* width: 100%; */
+  /* height: 100vh; */
   display: flex;
   flex-wrap: wrap;
-  align-content: flex-start;
   justify-content: center;
 
   height: min-content;
-  min-height: calc(100vh - 91px);
+  min-height: calc(100vh - 90px);
+
+  /* min-height: max-content; */
+  /* justify-content: center; */
+  /* flex-wrap: wrap; */
 `;
 
-const Searcher = () => {
+const Searcher = ({ changeVis }) => {
+  // const [allCards, setAllCards] = useState([]);
   const {
-    nft,
-    setNft,
-    marketCards,
-    setMarketCards,
     myInfos,
-    setMyInfos,
-    changed,
+    search,
+    setSearch,
+    marketCards,
+    filtered,
+    setFiltered,
+    change,
     setChanged,
-    readyToLoad,
   } = useContext(AuthContext);
 
-  // marketCards.sort(() => 0.5 - Math.random());
+  useEffect(() => {
+    if (change) {
+      setFiltered([]);
+      console.log("called");
+      const done = async () => {
+        const cards = myInfos.cards;
+        if (cards) {
+          const lowerSearch = search.toLowerCase();
+          //getting the creator of searched team
+          const clubAddress = await cards.methods
+            .clubToCreator(lowerSearch)
+            .call();
+
+          console.log(`clubaddress: ${clubAddress}`);
+          if (clubAddress === "0x0000000000000000000000000000000000000000")
+            return;
+          // const market = await cards.methods.marketCards().call();
+          // mapping(address => mapping(uint256 => Card)) public marketCards;
+
+          const balance = await cards.methods.balanceOf(cards._address).call();
+          for (let i = 0; i < balance; i++) {
+            const tokenId = await cards.methods
+              .tokenOfOwnerByIndex(cards._address, i)
+              .call();
+            const token = await cards.methods.tokenByIndex(tokenId).call();
+            const item = await cards.methods
+              .marketCards(cards._address, token)
+              .call();
+
+            console.log(`club address ${clubAddress}`);
+            if (
+              item.createdBy === "0xAf94f8a506be9b64C505Eb25Ffb8d2298a81D801"
+            ) {
+              setFiltered((filtered) => [...filtered, item]);
+            }
+
+            // setMyCards((myCards) => [...myCards, item]);
+          }
+        }
+      };
+      done();
+
+      console.log(filtered);
+
+      // setInterval(() => {
+      //   console.log(filtered);
+      // }, 1000);
+    }
+  }, [change]);
 
   return (
     <Container>
-      {marketCards.map((item, key) => {
-        return (
-          <div key={key}>
-            <Card card={item}></Card>
-          </div>
-        );
+      {filtered.map((item, key) => {
+        return <Card changeVis={changeVis} key={key} card={item}></Card>;
       })}
     </Container>
   );
