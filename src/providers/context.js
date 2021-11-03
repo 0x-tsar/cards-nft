8cbb0a80ecb31829f177b601cc92ec43dd9d523d
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-03 05:18:23 -0300

    some cool results

--- src/providers/context.js
+++ src/providers/context.js
@@ -18,81 +18,82 @@ export const AuthProvider = (props) => {
   const [refresh, setRefresh] = useState(false);
   // const [currentTab, setCurrentTab] = useState("");
 
-  useEffect(() => {
-    const done = async () => {
-      const { cards, web3 } = await loadEthereum();
-      if (cards && web3) {
-        //////////////////////
-        //////////////////////
-        //////////////////////
-        // reading users items
+  const done = async () => {
+    const { cards, web3 } = await loadEthereum();
+    if (cards && web3) {
+      //////////////////////
+      //////////////////////
+      //////////////////////
+      // reading users items
 
-        const account = await web3.currentProvider.selectedAddress;
-        const balanceEther = await web3.eth.getBalance(account);
-        setMyInfos({
-          account: account,
-          balanceEther: balanceEther,
-          cards: cards,
-          web3: web3,
-        });
+      const account = await web3.currentProvider.selectedAddress;
+      const balanceEther = await web3.eth.getBalance(account);
+      setMyInfos({
+        account: account,
+        balanceEther: balanceEther,
+        cards: cards,
+        web3: web3,
+      });
 
-        const balanceUser = await cards.methods.balanceOf(account).call();
-        console.log(`balance user: ${balanceUser}`);
-        for (let i = 0; i < balanceUser; i++) {
-          const tokenId = await cards.methods
-            .tokenOfOwnerByIndex(account, i)
-            .call();
-          const token = await cards.methods.tokenByIndex(tokenId).call();
-          const item = await cards.methods.myCards(account, token).call();
-          console.log(item);
+      const balanceUser = await cards.methods.balanceOf(account).call();
+      console.log(`balance user: ${balanceUser}`);
+      for (let i = 0; i < balanceUser; i++) {
+        const tokenId = await cards.methods
+          .tokenOfOwnerByIndex(account, i)
+          .call();
+        const token = await cards.methods.tokenByIndex(tokenId).call();
+        const item = await cards.methods.myCards(account, token).call();
+        console.log(item);
 
-          setMyCards((myCards) => [...myCards, item]);
-        }
-
-        const balance = await cards.methods.balanceOf(cards._address).call();
-        console.log(balance);
-        for (let i = 0; i < balance; i++) {
-          const tokenId = await cards.methods
-            .tokenOfOwnerByIndex(cards._address, i)
-            .call();
-          const token = await cards.methods.tokenByIndex(tokenId).call();
-          const item = await cards.methods
-            .marketCards(cards._address, token)
-            .call();
-
-          setMarketCards((marketCards) => [...marketCards, item]);
-        }
-
-        //events
-        //events
-        cards.events
-          .cardMinted({})
-          .on("data", async function (event) {
-            // console.log(event.returnValues);
-            // Do something here
-            setMarketCards((marketCards) => [
-              ...marketCards,
-              event.returnValues,
-            ]);
-          })
-          .on("error", console.error);
-
-        //
-        cards.events
-          .cardTransfered({})
-          .on("data", async function (event) {
-            // console.log(event.returnValues);
-            // Do something here
-            // changeVis("none");
-            window.location.reload();
-            // setMarketCards((marketCards) => [...marketCards, event.returnValues]);
-          })
-          .on("error", console.error);
+        setMyCards((myCards) => [...myCards, item]);
       }
-    };
 
+      const balance = await cards.methods.balanceOf(cards._address).call();
+      console.log(balance);
+      for (let i = 0; i < balance; i++) {
+        const tokenId = await cards.methods
+          .tokenOfOwnerByIndex(cards._address, i)
+          .call();
+        const token = await cards.methods.tokenByIndex(tokenId).call();
+        const item = await cards.methods
+          .marketCards(cards._address, token)
+          .call();
+
+        setMarketCards((marketCards) => [...marketCards, item]);
+      }
+
+      //events
+      //events
+      cards.events
+        .cardMinted({})
+        .on("data", async function (event) {
+          // console.log(event.returnValues);
+          // Do something here
+          setMarketCards((marketCards) => [...marketCards, event.returnValues]);
+        })
+        .on("error", console.error);
+
+      //
+      cards.events
+        .cardTransfered({})
+        .on("data", async function (event) {
+          // console.log(event.returnValues);
+          // Do something here
+          // changeVis("none");
+          window.location.reload();
+          // setMarketCards((marketCards) => [...marketCards, event.returnValues]);
+        })
+        .on("error", console.error);
+    }
+  };
+
+  // useEffect(() => {
+  //   done();
+  // }, []);
+
+  useEffect(() => {
     done();
-  }, []);
+  }, [refresh]);
 
   return (
     <AuthContext.Provider
