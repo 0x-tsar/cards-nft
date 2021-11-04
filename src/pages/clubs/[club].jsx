b15be9e69cb947e19a39add26f84c77f4ff1c6fe
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-04 02:43:04 -0300

    checkpoint

--- src/pages/clubs/[club].jsx
+++ src/pages/clubs/[club].jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import { AuthContext } from "../../providers/context";
+import Card from "../../Components/Card";
+import loadEthereum from "../../ethereum";
 
 export const Container = styled.div`
   grid-area: main;
@@ -18,6 +21,62 @@ export const Container = styled.div`
 const Club = () => {
   const router = useRouter();
   const { club } = router.query;
+
+  const [clubCards, setClubCards] = useState([]);
+
+  const {
+    nft,
+    setNft,
+    marketCards,
+    setMarketCards,
+    myInfos,
+    setMyInfos,
+    myCards,
+    setMyCards,
+    search,
+    setSearch,
+    changed,
+    setChanged,
+    isLoading,
+    setIsLoading,
+    readyToLoad,
+    currentTab,
+    setCurrentTab,
+    whichTab,
+    setWhichTab,
+  } = useContext(AuthContext);
+
+  const done = async () => {
+    const { cards, web3 } = await loadEthereum();
+    console.log(cards);
+    if (cards && web3) {
+      const account = await web3.currentProvider.selectedAddress;
+      const balanceEther = await web3.eth.getBalance(account);
+
+      console.log(club);
+      // const thisCards = await cards.methods.clubToCreator(club).call();
+      // console.log(thisCards);
+      // const balanceUser = await cards.methods.balanceOf(account).call();
+    }
+  };
+
+  done();
+
+  return (
+    <Container>
+      {clubCards.map((item, key) => {
+        return (
+          <div key={key}>
+            <Card
+              card={item}
+              // changeVis={changeVis}
+              which={"clubs"}
+            ></Card>
+          </div>
+        );
+      })}
+    </Container>
+  );
 
   return <Container>HELLO FROM CLUB: {club}</Container>;
 };
